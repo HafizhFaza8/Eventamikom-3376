16623a4188737f273db449d15125008c6d89211f
HafizhFaza8 <hafizhfaza80@gmail.com>
2026-05-08 10:27:12 +0700

    Tugas 4

--- README.md
+++ README.md
@@ -19,6 +19,9 @@ KONTAK : https://eventamikom-3376-main-bjm8ma.free.laravel.cloud/kontak<br>
 
 <h2>Tugas 3 </h2>
 Menambahkan database migration/seeding
+
+<h2>Tugas 4 </h2>
+CRUD Tabel Event
 <p align="center"><a href="https://laravel.com" target="_blank"><img src="https://raw.githubusercontent.com/laravel/art/master/logo-lockup/5%20SVG/2%20CMYK/1%20Full%20Color/laravel-logolockup-cmyk-red.svg" width="400" alt="Laravel Logo"></a></p>
 
 <p align="center">
--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\Admin\DashboardController;
-use App\Http\Controllers\Admin\CategoryController; 
-use App\Http\Controllers\Admin\EventsController; 
+use App\Http\Controllers\Admin\CategoryController;  
+use App\Http\Controllers\Admin\EventController as EventAdminController;
 
 // --- Rute User Area ---
 Route::get('/', [HomeController::class, 'index'])->name('home');
@@ -27,9 +27,6 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
     // Halaman Dashboard Utama Admin
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
     
-    // Halaman Kelola Event
-    Route::get('/events', [EventsController::class, 'index'])->name('events');
-    
     // Halaman Laporan Transaksi
     Route::get('/transactions', function () {
         return view('admin.transactions');
@@ -37,4 +34,13 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
 
     // TUGAS PERTEMUAN 3: Manajemen Kategori 
     Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
+
+    // --- MANAJEMEN EVENT ---
+    // Gunakan Route::resource karena otomatis membuatkan rute admin.events.index, admin.events.create, dll.
+    Route::resource('events', EventAdminController::class);
+    
+    // Catatan: Saya telah menghapus Route::get('/events') manual dan grup admin dobel 
+    // agar tidak terjadi bentrok (conflict) dengan Route::resource di atas.
+
 });
+
